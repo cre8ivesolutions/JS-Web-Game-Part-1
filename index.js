970963fdf48e80d5dee99cc2adcd7ac861e1cb7b
Tammy Swierczynski <tammerz78@gmail.com>
2022-06-01 14:29:57 -0500

    first today

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'  
-document.body.append(greenCharacter)
+function background(imageLocation, leftPosition, bottomPosition, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(imageLocation, leftPosition + w*100, bottomPosition+h*100)
+        }
+    }
+}
+
+function newImage(imageLocation, leftPosition, bottomPosition){
+    let image = document.createElement('img')
+        image.src = imageLocation
+        image.style.position = 'fixed'
+        image.style.left = leftPosition + 'px'
+        image.style.bottom = bottomPosition + 'px'  
+        document.body.append(image)
+    return image
+}
+
+function newItem(imageLocation, leftPosition, bottomPosition ){
+    let object = newImage(imageLocation, leftPosition, bottomPosition)
+ 
+    object.addeventlistenter('click', () => {
+        object.remove()
+    })
+}
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+background('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+background('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300 )
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'  
+// document.body.append(greenCharacter)
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'  
+// document.body.append(pineTree)
